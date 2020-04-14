e1e41202ba69ff0e1ebe5b617b1b2f5a1d8112cf
sumedh10 <sumedhpanchadhar@gmail.com>
2020-04-13 22:36:32 -0400

    test without dplyr and stringr

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,46 +1,27 @@
-library(stringr)
-library(dplyr)
-
 test_vector <- c("B", "b")
-expected_vector <- c("b", "B")
+expected_vector_1 <- c("b", "B")
+expected_vector_2 <- 2:1
 
-test_df <- data.frame(test_col = test_vector, stringsAsFactors = FALSE)
-expected_df <- data.frame(test_col = expected_vector, stringsAsFactors = FALSE)
-
-
-test_that("dplyr::arrange()", {
-
-  arrange_df <- arrange(test_df, test_col)
-
-  cat("\n df - arrange \n")
-  print(arrange_df)
-
-  cat("\n Expected result \n")
-  print(expected_df)
-
-  expect_equal(arrange_df, expected_df)
-  # The test will pass if we use a tibble instead of a dataframe because
-  # dplyr's masked version of all.equal() ignores row order by default
-})
-
-
-test_that("base::sort() vs stringr::str_sort()", {
+test_that("base::sort()", {
 
   sort_base_r <- sort(test_vector)
-  sort_stringr <- str_sort(test_vector)
-
-  cat("\n--------------------\n")
   cat("\n Base R - sort \n")
   print(sort_base_r)
 
-  cat("\n stringr - sort \n")
-  print(sort_stringr)
+  cat("\n Expected result \n")
+  print(expected_vector_1)
+
+  cat("\n--------------------\n")
+
+  order_base_r <- order(test_vector)
+
+  cat("\n Base R - order \n")
+  print(order_base_r)
 
   cat("\n Expected result \n")
-  print(expected_vector)
+  print(expected_vector_2)
 
-  expect_equal(sort_base_r, expected_vector)
-  # stringr::str_sort() works as expected
-  expect_equal(sort_stringr, expected_vector)
+  expect_equal(sort_base_r, expected_vector_1)
+  expect_equal(order_base_r, expected_vector_2)
 
 })
